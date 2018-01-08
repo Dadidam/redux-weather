99a9e3e384cb931a590a239356c3d8b43aa95f61
iliav <iliav@hurdit.com>
2018-01-08 17:25:16 -0500

    Added WeatherList container

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,10 +1,10 @@
-import FETCH_WEATHER from '../actions/index';
+import { FETCH_WEATHER } from '../actions/index';
 
-export default function(state = [], action) {
+export default function (state = [], action) {
   switch (action.type) {
     case FETCH_WEATHER:
       // Dont' mutate current state!
-      return [ action.payload.data, ...state ];
+      return [action.payload.data, ...state];
   }
 
   return state;
