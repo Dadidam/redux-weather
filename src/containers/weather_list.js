731d8eaaeb0a192e2304eddbdc13575010886f17
iliav <iliav@hurdit.com>
2018-01-08 18:05:28 -0500

    Created dummy component for rendering charts

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import Chart from '../components/chart';
 
 class WeatherList extends Component {
     renderWeather(cityData) {
         const temps = cityData.list.map(weather => weather.main.temp);
+        const pressures = cityData.list.map(weather => weather.main.pressure);
+        const humidities = cityData.list.map(weather => weather.main.humidity);
 
         return (
             <tr key={cityData.city.id}>
                 <td>{cityData.city.name}</td>
-                <td>
-                    <Sparklines data={temps} height={120} width={180}>
-                        <SparklinesLine color="blue" />
-                    </Sparklines>
-                </td>
+                <td><Chart data={temps} color="red" /></td>
+                <td><Chart data={pressures} color="blue" /></td>
+                <td><Chart data={humidities} color="orange" /></td>
             </tr>
         );
     }
